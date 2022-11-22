a9be2faed0616854a7c525d88ae4c4ca243b1616
Manoj Dhami <105317632+Dmanoj07@users.noreply.github.com>
2022-11-22 00:17:27 -0500

    clerk controller setup

--- controllers/clerk.js
+++ controllers/clerk.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-router.get('/list-mealkits', (req, res) => {
+router.get('/list-mealkits', (req, res) => { //clerk controller and checking user session 
   if (req.session.user) {
     if (req.session.user.isclerk) {
       res.render("general/wcClerk.hbs");
